51ee39592289643576de19cdc2db99eb91603de9
Mac Ryan <quasipedia@gmail.com>
2016-09-03 21:34:32 +0200

    Initial commit with boilerplate

---
+++ spec/unsplit-panes-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import UnsplitPanesView from '../lib/unsplit-panes-view';
+
+describe('UnsplitPanesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
