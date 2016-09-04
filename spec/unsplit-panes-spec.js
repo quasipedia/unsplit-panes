21594e45eb1d2ae9cfce1d41bb0ec51fe42b3c32
Mac Ryan <quasipedia@gmail.com>
2016-09-04 22:29:11 +0200

    Cleanup of boilerplate

--- spec/unsplit-panes-spec.js
+++ spec/unsplit-panes-spec.js
@@ -7,67 +7,67 @@ import UnsplitPanes from '../lib/unsplit-panes';
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe('UnsplitPanes', () => {
-  let workspaceElement, activationPromise;
-
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('unsplit-panes');
-  });
-
-  describe('when the unsplit-panes:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.unsplit-panes')).not.toExist();
-
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'unsplit-panes:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.unsplit-panes')).toExist();
-
-        let unsplitPanesElement = workspaceElement.querySelector('.unsplit-panes');
-        expect(unsplitPanesElement).toExist();
-
-        let unsplitPanesPanel = atom.workspace.panelForItem(unsplitPanesElement);
-        expect(unsplitPanesPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'unsplit-panes:toggle');
-        expect(unsplitPanesPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.unsplit-panes')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'unsplit-panes:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let unsplitPanesElement = workspaceElement.querySelector('.unsplit-panes');
-        expect(unsplitPanesElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'unsplit-panes:toggle');
-        expect(unsplitPanesElement).not.toBeVisible();
-      });
-    });
-  });
-});
+// describe('UnsplitPanes', () => {
+//   let workspaceElement, activationPromise;
+//
+//   beforeEach(() => {
+//     workspaceElement = atom.views.getView(atom.workspace);
+//     activationPromise = atom.packages.activatePackage('unsplit-panes');
+//   });
+//
+//   describe('when the unsplit-panes:toggle event is triggered', () => {
+//     it('hides and shows the modal panel', () => {
+//       // Before the activation event the view is not on the DOM, and no panel
+//       // has been created
+//       expect(workspaceElement.querySelector('.unsplit-panes')).not.toExist();
+//
+//       // This is an activation event, triggering it will cause the package to be
+//       // activated.
+//       atom.commands.dispatch(workspaceElement, 'unsplit-panes:toggle');
+//
+//       waitsForPromise(() => {
+//         return activationPromise;
+//       });
+//
+//       runs(() => {
+//         expect(workspaceElement.querySelector('.unsplit-panes')).toExist();
+//
+//         let unsplitPanesElement = workspaceElement.querySelector('.unsplit-panes');
+//         expect(unsplitPanesElement).toExist();
+//
+//         let unsplitPanesPanel = atom.workspace.panelForItem(unsplitPanesElement);
+//         expect(unsplitPanesPanel.isVisible()).toBe(true);
+//         atom.commands.dispatch(workspaceElement, 'unsplit-panes:toggle');
+//         expect(unsplitPanesPanel.isVisible()).toBe(false);
+//       });
+//     });
+//
+//     it('hides and shows the view', () => {
+//       // This test shows you an integration test testing at the view level.
+//
+//       // Attaching the workspaceElement to the DOM is required to allow the
+//       // `toBeVisible()` matchers to work. Anything testing visibility or focus
+//       // requires that the workspaceElement is on the DOM. Tests that attach the
+//       // workspaceElement to the DOM are generally slower than those off DOM.
+//       jasmine.attachToDOM(workspaceElement);
+//
+//       expect(workspaceElement.querySelector('.unsplit-panes')).not.toExist();
+//
+//       // This is an activation event, triggering it causes the package to be
+//       // activated.
+//       atom.commands.dispatch(workspaceElement, 'unsplit-panes:toggle');
+//
+//       waitsForPromise(() => {
+//         return activationPromise;
+//       });
+//
+//       runs(() => {
+//         // Now we can test for view visibility
+//         let unsplitPanesElement = workspaceElement.querySelector('.unsplit-panes');
+//         expect(unsplitPanesElement).toBeVisible();
+//         atom.commands.dispatch(workspaceElement, 'unsplit-panes:toggle');
+//         expect(unsplitPanesElement).not.toBeVisible();
+//       });
+//     });
+//   });
+// });
--- spec/unsplit-panes-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import UnsplitPanesView from '../lib/unsplit-panes-view';
-
-describe('UnsplitPanesView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
